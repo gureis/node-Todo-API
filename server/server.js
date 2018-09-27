bedbee4d935770f54d27c6dd01ecfed99f03a297
gureis <gureis4@gmail.com>
2018-09-27 16:07:58 -0300

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const {ObjectID} = require('mongodb')
 
 const {mongoose} = require('./db/mongoose')
 const {Users} = require('./models/user')
@@ -36,6 +37,18 @@ app.post('/users', (req, res) => {
 app.get('/todos', (req, res) => {
     Todos.find({})
         .then(todos => res.send({todos}), e => res.status(400).send(e));
+});
+
+app.get(`/todos/:id`, (req, res) => {
+    const id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+        return res.status(400).send({error: `ID is Invalid`});
+    }
+    Todos.findById(id).then(todo => {
+        if(!todo)
+            return res.status(404).send({error: `Todo not Found`});
+        res.status(200).send({ todo });
+    }).catch(e => res.status(400).send());
 });
 
 app.get('/users', (req, res) => {
