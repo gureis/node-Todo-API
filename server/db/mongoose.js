75f9a8772fb8acbfbade6127669425b741fc4df6
gureis <gureis4@gmail.com>
2018-09-27 17:12:42 -0300

    mongo path heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds117623.mlab.com:17623/todoapp-db', { useNewUrlParser: true });
+mongoose.connect('mongodb://<admin>:<th3@w3Som3dB>@ds117623.mlab.com:17623/todoapp-db', { useNewUrlParser: true });
 
 module.exports = {
     mongoose
