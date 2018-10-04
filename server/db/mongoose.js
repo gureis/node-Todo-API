0ddb3cd828df5dca8570fe576e44bbceb583eddf
gureis <gureis4@gmail.com>
2018-10-03 21:13:11 -0300

    fix mLab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
-const mlab = 'mongodb://gurei:th3@w3Som3dB@ds117623.mlab.com:17623/todoapp-db';
-const local = 'mongodb://localhost:27017/TodoApp'
+const mlab = 'mongodb://gurei:th3Aw3Som3dB@ds117623.mlab.com:17623/todoapp-db';
+const local = 'mongodb://localhost:27017/TodoApp';
 
-mongoose.connect(mlab, { useNewUrlParser: true });
+mongoose.connect(mlab);
 
 module.exports = {
     mongoose
