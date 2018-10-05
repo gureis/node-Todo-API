c2d2dae1deb92b5657c6045c9394f8203b972ca7
gureis <gureis4@gmail.com>
2018-10-04 22:40:17 -0300

    mlab connect

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const mlab = 'mongodb://gurei:th3Aw3Som3dB@ds117623.mlab.com:17623/todoapp-db';
 // const local = 'mongodb://localhost:27017/TodoApp';
 
-mongoose.connect(local, { useNewUrlParser: true });
+mongoose.connect(mlab, { useNewUrlParser: true });
 
 module.exports = {
     mongoose
